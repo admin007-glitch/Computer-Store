3fe26f98e0a713e1bad6e50fa1b19278719550b9
admin007-glitch <super.shazam@bk.ru>
2025-06-10 18:59:43 +0600

    Update API URLs for production

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,7 +46,7 @@ function App() {
   );
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/products')
+  fetch(`${process.env.REACT_APP_API_URL}/api/products`)
       .then(res => res.json())
       .then(data => setProducts(data))
       .catch(err => console.error(err));
